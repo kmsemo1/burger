3d94544d7bf205601fdb8ee3b5d972b6fa0c639f
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-01-13 20:02:29 -0800

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Southwest Burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Plain Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Breakfast Burger", true);
